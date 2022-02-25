22ded6e991f0338abe7a563fd22b67e374fa0379
Thomas Zerr <thomas.zerr.3@hof-university.de>
2022-02-25 14:02:14 +0100

    -added: links to posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import { getSortedPostsData, PostData } from '../lib/posts'
 import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import Date from '../components/date'
+
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -30,11 +33,13 @@ export default function Home({allPostsData} : {allPostsData: PostData[]}) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date.toString()} />
+              </small>
             </li>
           ))}
         </ul>
